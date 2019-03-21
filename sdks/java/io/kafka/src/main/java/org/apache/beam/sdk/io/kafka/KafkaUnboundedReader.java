024cfd996808ef01465bebbf80815219c8e8676a
taegeonum <taegeonum@gmail.com>
2019-03-21 17:29:02 +0900

    log

--- sdks/java/io/kafka/src/main/java/org/apache/beam/sdk/io/kafka/KafkaUnboundedReader.java
+++ sdks/java/io/kafka/src/main/java/org/apache/beam/sdk/io/kafka/KafkaUnboundedReader.java
@@ -261,6 +261,7 @@ class KafkaUnboundedReader<K, V> extends UnboundedReader<KafkaRecord<K, V>> {
     if (source.getSpec().getWatermarkFn() != null) {
       // Support old API which requires a KafkaRecord to invoke watermarkFn.
       //LOG.info("{}: getWatermark() : no records have been read yet.", name);
+      LOG.info("geWatermarkFn: {}", source.getSpec().getWatermarkFn().getClass().getName());
       if (curRecord == null) {
         LOG.debug("{}: getWatermark() : no records have been read yet.", name);
         return initialWatermark;

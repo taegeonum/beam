0d03172339b37d1d480da879743a5b6c59b73d3e
taegeonum <taegeonum@gmail.com>
2019-03-22 08:49:47 +0900

    rm logging

--- sdks/java/nexmark/src/main/java/org/apache/beam/sdk/nexmark/KafkaTimestampPolicy.java
+++ sdks/java/nexmark/src/main/java/org/apache/beam/sdk/nexmark/KafkaTimestampPolicy.java
@@ -15,10 +15,10 @@ public class KafkaTimestampPolicy extends TimestampPolicy implements Serializabl
 
     @Override
     public Instant getTimestampForRecord(PartitionContext ctx, KafkaRecord record) {
-        LOG.info("GetTimestamp: {}", this);
+        //LOG.info("GetTimestamp: {}", this);
 
         if (record.getTimestamp() > timestamp) {
-            LOG.info("Timestamp set: {}", record.getTimestamp());
+            //LOG.info("Timestamp set: {}", record.getTimestamp());
             timestamp = record.getTimestamp();
         }
         return new Instant(record.getTimestamp());
@@ -26,7 +26,7 @@ public class KafkaTimestampPolicy extends TimestampPolicy implements Serializabl
 
     @Override
     public Instant getWatermark(PartitionContext ctx) {
-        LOG.info("GetWatermark {}", this);
+        //LOG.info("GetWatermark {}", this);
         return new Instant(timestamp);
     }
 }
--- sdks/java/nexmark/src/main/java/org/apache/beam/sdk/nexmark/KafkaTimestampPolicyFactory.java
+++ sdks/java/nexmark/src/main/java/org/apache/beam/sdk/nexmark/KafkaTimestampPolicyFactory.java
@@ -1,9 +1,13 @@
 package org.apache.beam.sdk.nexmark;
 
+import org.apache.beam.sdk.io.kafka.CustomTimestampPolicyWithLimitedDelay;
+import org.apache.beam.sdk.io.kafka.KafkaRecord;
 import org.apache.beam.sdk.io.kafka.TimestampPolicy;
 import org.apache.beam.sdk.io.kafka.TimestampPolicyFactory;
 import org.apache.beam.sdk.nexmark.model.Event;
+import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.kafka.common.TopicPartition;
+import org.joda.time.Duration;
 import org.joda.time.Instant;
 
 import java.io.Serializable;
@@ -13,6 +17,15 @@ import java.util.Optional;
 public class KafkaTimestampPolicyFactory implements Serializable, TimestampPolicyFactory<Long, Event> {
     @Override
     public TimestampPolicy<Long, Event> createTimestampPolicy(TopicPartition tp, Optional<Instant> previousWatermark) {
+        //return new CustomTimestampPolicyWithLimitedDelay(new TimestampFunc(), Duration.standardSeconds(3), Optional.empty());
         return new KafkaTimestampPolicy();
+    }
+
+    public static final class TimestampFunc implements SerializableFunction<KafkaRecord<Long, Event>, Instant> {
+
+        @Override
+        public Instant apply(KafkaRecord<Long, Event> input) {
+            return new Instant(input.getTimestamp());
+        }
     }
 }
